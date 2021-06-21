608469f7cd4a335e675fdfd8a74ffabf60748421
lukasz <lukaszkuc93@gmail.com>
2021-06-21 16:21:42 +0200

    Stworzenie widoku rejestracji uzytkownika JFDDR3PN-3

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SignUp from "./SignUp";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div>
-      <h1>Money Control</h1>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className='w-100' style={{ maxWidth: "400px"}} >
+        <SignUp />
+      </div>
+    </Container>
   );
 }
 
