a6ceac8d625eaf09fdc07d7c2439af09a4e38327
Piotr Krasuski <krasuskip@gmail.com>
2021-06-23 11:30:56 +0200

    Migrate to Material UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './views/App/App.js'
+import SignIn from './views/SignIn/SignIn';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
