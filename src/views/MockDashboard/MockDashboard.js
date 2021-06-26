7ec35356f41719f26bbab9af6baf4925d86e1c28
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-26 17:11:25 +0200

    added signOut functionality

--- src/views/MockDashboard/MockDashboard.js
+++ src/views/MockDashboard/MockDashboard.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Typography, Button } from '@material-ui/core';
+import { useAuth } from '../../contexts/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 function MockDashboard() {
+  const [error, setError] = useState('');
+  const { currentUser, signOut } = useAuth();
+  const history = useHistory();
+
+  async function handleSignOut() {
+    setError('');
+
+    try {
+      await signOut();
+      history.push('/signin');
+    } catch {
+      setError('Failed to log out');
+    }
+  }
+
   return (
     <Container>
+      {error && <p>{error}</p>}
       <Typography>MockDashboard</Typography>
-      <Typography>Welcome: ...</Typography>
-      <Button variant="contained" color="primary">
+      <Typography>Welcome: {currentUser.email}</Typography>
+      <Button variant="contained" color="primary" onClick={handleSignOut}>
         wyloguj
       </Button>
     </Container>
