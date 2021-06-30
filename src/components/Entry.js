7a37c39b8b54d721c47d02f47b83d4c0918bea22
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-30 23:32:05 +0200

    feat: added delete entry functionality

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { firestore } from '../config/firebase';
+import { database } from '../config/firebase';
 import styled from 'styled-components';
 
 export const Entry = ({ entry }) => {
+  const onDelete = e => {
+    database.entries.doc(entry.id).delete();
+  };
+
   return (
     <li>
       [{entry.type}] [{entry.category}] [{entry.note}] [{entry.date}] [
       {entry.amount}]<button>Update</button>
-      <button>Delete</button>
+      <button onClick={onDelete}>Delete</button>
     </li>
   );
 };
