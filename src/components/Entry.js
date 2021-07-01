9738490ae3147cda98403e2b00c5c9b34088f033
Konrad Roguszewski <aker.hm@gmail.com>
2021-07-01 17:27:11 +0200

    feat: conditional link to update view

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import { database } from '../config/firebase';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Entry = ({ entry }) => {
-  const onDelete = e => {
+  const onDelete = () => {
     database.entries.doc(entry.id).delete();
   };
+
+  // console.log(database.entries.doc(entry.id))
 
   return (
     <li>
       [{entry.type}] [{entry.category}] [{entry.note}] [{entry.date}] [
-      {entry.amount}]<button>Update</button>
+      {entry.amount}]
+      {entry.type === 'income' ? (
+        <Link
+          to={{
+            pathname: '/add-entry',
+            state: {
+              entry,
+              operation: 'Update',
+              type: 'income',
+              options: ['Work', 'Gifts', 'Other']
+            }
+          }}
+        >
+          Update
+        </Link>
+      ) : (
+        <Link
+          to={{
+            pathname: '/add-entry',
+            state: {
+              entry,
+              operation: 'Update',
+              type: 'expense',
+              options: [
+                'Food',
+                'Transport',
+                'Accomodation',
+                'Entertainment',
+                'Other'
+              ]
+            }
+          }}
+        >
+          Update
+        </Link>
+      )}
       <button onClick={onDelete}>Delete</button>
     </li>
   );
