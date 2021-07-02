d66ef953de328450f07968ed527d7a32e494acd5
Zygmunt Karolina <zygmunt.karolinazygmunt.karoli@gmail.com>
2021-07-02 10:19:03 +0200

    dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-// import { borders } from '@material-ui/system';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useAuth } from '../../contexts/AuthContext';
+import { useHistory } from 'react-router-dom';
+import { Container, Button , Avatar } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(2)
-    }
-  }
-}));
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+
+const StyledAvatar = styled(Avatar)`
+  background-color: #156a77;
+  margin-right: 10px;
+`;
+
+const StyledPageTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const StyledContainer = styled(Container)`
   border: 1px solid #000;
-  width: 400px;
+  width: 50% !important;
   text-align: center;
+  & > * {
+    margin: 2px;
+  }
+  button {
+    margin: 16px;
+    background-color: #156a77;
+  }
 `;
 
-const StyledLogout = styled(Button)``;
+const StyledLogout = styled(Button)`
+  background-color: #156a77;
+`;
 
 const StyledBox = styled.div`
   display: flex;
@@ -32,28 +44,64 @@ const StyledBox = styled.div`
   }
 `;
 
+const StyledButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
 function Dashboard() {
-  const classes = useStyles();
+  const [error, setError] = useState('');
+  const { currentUser, signOut } = useAuth();
+  const history = useHistory();
+
+  async function handleSignOut() {
+    setError('');
+
+    try {
+      await signOut();
+      history.push('/signin');
+    } catch {
+      setError('Failed to log out');
+    }
+  }
 
   return (
-    <StyledContainer className={classes.root}>
+    <StyledContainer>
       <StyledBox>
         <div>
-          <h3>Money Control</h3>
-          <h3>welcome, Username</h3>
+          <StyledPageTitle>
+            <StyledAvatar>
+              <LockOpenOutlinedIcon />
+            </StyledAvatar>
+            <h3>Money Control</h3>
+          </StyledPageTitle>
+          <h3>welcome: {currentUser.email}</h3>
         </div>
-        <StyledLogout variant="contained" color="primary" disableElevation>
+        <StyledLogout
+          variant="contained"
+          color="primary"
+          onClick={handleSignOut}
+        >
           logout
         </StyledLogout>
       </StyledBox>
-      <h3>&laquo; &laquo; Month &raquo; &raquo;</h3>
+      <h3>
+        {' '}
+        <a href="#">&laquo;</a> Month <a href="#">&raquo;</a>
+      </h3>
       <h3> no current records</h3>
-      <Button variant="contained" color="primary" disableElevation>
-        new expense
-      </Button>
-      <Button variant="contained" color="primary" disableElevation>
-        new income
-      </Button>
+      <StyledButtons>
+        <Button variant="contained" color="primary" disableElevation>
+          new expense
+        </Button>
+        <Button variant="contained" color="primary" disableElevation>
+          new income
+        </Button>
+      </StyledButtons>
     </StyledContainer>
   );
 }
