a2ca38c0f37a3cd5b0ddc15335c6f7c410d22ce0
Konrad Roguszewski <aker.hm@gmail.com>
2021-07-10 12:40:09 +0200

    feat: dynamic focus of TypeSwitch

--- src/components/TypeSwitch.js
+++ src/components/TypeSwitch.js
@@ -1,42 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '@material-ui/core';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-const StyledButton = styled(Button)`
-  background-color: #156a77;
-  color: white;
+const StyledButton = styled(Button)(props => ({
+  background: props.background,
+  color: 'white'
+}));
+
+const StyledButtonGroup = styled(ButtonGroup)`
+  color: #156a77;
 `;
 
-export const TypeSwitch = ({ setter }) => {
-  const [alignment, setAlignment] = React.useState('all');
+const ButtonsContainer = styled.div``;
 
-  const handleAlignment = (event, newAlignment) => {
-    setAlignment(newAlignment);
+export const TypeSwitch = ({ setter }) => {
+  const [focus, setFocus] = useState('all');
+
+  const handleFocus = newFocus => {
+    setFocus(newFocus);
   };
 
   return (
-    <div value={alignment} onChange={handleAlignment}>
-      <StyledButton
-        value="all"
-        onClick={() => setter('all')}
-        variant="contained"
+    <ButtonsContainer value={focus} onChange={handleFocus}>
+      <StyledButtonGroup
+        color="primary"
+        aria-label="outlined primary button group"
       >
-        All
-      </StyledButton>
-      <StyledButton
-        value="incomes"
-        onClick={() => setter('incomes')}
-        variant="contained"
-      >
-        Incomes
-      </StyledButton>
-      <StyledButton
-        value="expenses"
-        onClick={() => setter('expenses')}
-        variant="contained"
-      >
-        Expenses
-      </StyledButton>
-    </div>
+        <StyledButton
+          onClick={() => {
+            setter('all');
+            setFocus('all');
+          }}
+          variant="contained"
+          background={focus === 'all' ? '#156a77' : '#808080b3'}
+          style={{ marginRight: 0 }}
+        >
+          All
+        </StyledButton>
+        <StyledButton
+          onClick={() => {
+            setter('incomes');
+            setFocus('incomes');
+          }}
+          variant="contained"
+          background={focus === 'incomes' ? '#156a77' : '#808080b3'}
+          style={{ marginRight: 0 }}
+        >
+          Incomes
+        </StyledButton>
+        <StyledButton
+          onClick={() => {
+            setter('expenses');
+            setFocus('expenses');
+          }}
+          variant="contained"
+          background={focus === 'expenses' ? '#156a77' : '#808080b3'}
+          style={{ marginRight: 0 }}
+        >
+          Expenses
+        </StyledButton>
+      </StyledButtonGroup>
+    </ButtonsContainer>
   );
 };
