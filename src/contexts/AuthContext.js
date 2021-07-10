e8f06f605c469d66b33c4cb211503a452ea892cc
Konrad Roguszewski <aker.hm@gmail.com>
2021-07-10 18:58:24 +0200

    feat: changes to AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -23,6 +23,14 @@ export function AuthProvider({ children }) {
     return auth.signOut();
   }
 
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user);
@@ -36,7 +44,9 @@ export function AuthProvider({ children }) {
     currentUser,
     signUp,
     signIn,
-    signOut
+    signOut,
+    updateEmail,
+    updatePassword
   };
 
   return (
